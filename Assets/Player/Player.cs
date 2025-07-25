0e0df40d4503320507aea2cac96e2939fafffb1f
Kaspar Moberg <kaspeti123@gmail.com>
2025-07-25 23:54:12 +0200

    chore: removed Debug msg in Player.cs

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -4,13 +4,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private string playerName;
     
-    private PlayerStats _playerStats;
+    private PlayerStats _playerStats; 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         _playerStats = PlayerStatsManager.LoadStats(playerName);
-        Debug.Log(_playerStats.playerName + _playerStats.vision);
     }
 
     // Update is called once per frame
